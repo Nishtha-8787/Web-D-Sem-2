40c5c8cc40c98271c2ee8be4212043c2c93afeb7
Nishtha-8787 <yadavnishtha033@gmail.com>
2026-06-18 21:23:03 +0530

    Add files via upload

---
+++ exp2.js
@@ -0,0 +1,11 @@
+let employee = [];
+function addemployee() {
+    let name = document.getElementById("name").value;
+    let empId = document.getElementById("empId").value;
+    let salary = parseFloat(document.getElementById("salary")).value;
+    let dept = document.getElementById("dept").value;
+    if (name === "" || empId === "" || isNaN(salary) || dept
+
+
+}
+let employee = 
